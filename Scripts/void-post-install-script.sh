dfb7a4de33bf4189c3cfae4f1f91f55edfa54ea5
juipeltje <joppe4444@outlook.com>
2023-09-18 17:06:37 +0200

    adding fstrim section and sshd to the services section of the post-install script, plus sleep commands to slow down script, and a set -e option

--- Scripts/void-post-install-script.sh
+++ Scripts/void-post-install-script.sh
@@ -3,26 +3,27 @@
 # post install script for void linux
 # NOTE TO SELF: MAKE SURE TO SETUP YOUR GIT ACCOUNT AND SSH KEY BEFORE RUNNING THIS SCRIPT!!
 
-set -x
+set -xe
 
 echo "starting void linux post install script..."
+sleep 1
 
 # update package manager
 
 echo "updating package manager..."
-
+sleep 1
 xbps-install -uy xbps
 
 # updating the system
 
 echo "performing system update..."
-
+sleep 1
 xbps-install -Suy
 
 # enabling additional repositories
 
 echo "enabling additional repositories..."
-
+sleep 1
 xbps-install -Sy \
 void-repo-multilib	\
 void-repo-multilib-nonfree	\
@@ -31,9 +32,8 @@ void-repo-nonfree
 # installing packages
 
 echo "installing packages..."
-
+sleep 1
 xbps-install -Sy \
-  # display server and graphics drivers
 xorg	\
 vulkan-loader	\
 mesa	\
@@ -44,7 +44,6 @@ mesa-vaapi	\
 mesa-vdpau	\
 mesa-vulkan-radeon	\
 mesa-vulkan-radeon-32bit	\
-  # DE
 qtile	\
 dunst	\
 rofi	\
@@ -65,7 +64,6 @@ network-manager-applet	\
 blueman	\
 papirus-folders	\
 papirus-icon-theme	\
-  # display manager/login tools
 lightdm	\
 lightdm-gtk3-greeter	\
 lightdm-webkit2-greeter	\
@@ -73,7 +71,6 @@ elogind	\
 dbus-elogind    \
 dbus-elogind-libs       \
 dbus-elogind-x11        \
-  # Multimedia
 pipewire	\
 wireplumber	\
 playerctl	\
@@ -82,7 +79,6 @@ mpv	\
 feh	\
 strawberry	\
 qbittorrent	\
-  # system tools
 nano	\
 socklog-void	\
 wireguard-dkms	\
@@ -114,7 +110,6 @@ gvfs	\
 fuse    \
 fuse3   \
 i2c-tools	\
-  # gaming
 wine	\
 mono	\
 wine-mono	\
@@ -124,7 +119,6 @@ MangoHud	\
 PrismLauncher	\
 openjdk17-jre	\
 dolphin-emu	\
-  # virtualization
 qemu	\
 libvirt	\
 virt-manager	\
@@ -134,16 +128,19 @@ iptables	\
 # setting up xbps-src
 
 echo "setting up xbps-src..."
-
+sleep 1
 git clone https://github.com/void-linux/void-packages.git
+sleep 1
 cd void-packages
+sleep 1
 ./xbps-src binary-bootstrap
+sleep 1
 cd
 
 # installing pip and python packages/dependencies
 
 echo "installing pip and python packages/dependencies..."
-
+sleep 1
 xbps-install -Sy 	\
 python3	\
 python3-pip	\
@@ -157,143 +154,217 @@ python3-Pillow	\
 python3-smbus	\
 libusb	\
 python3-setuptools
-
+sleep 1
 python -m venv /home/joppe/python-venv
+sleep 1
 /home/joppe/python-venv/bin/python -m pip install dbus-next
+sleep 1
 /home/joppe/python-venv/bin/python -m pip install liquidctl
 
 # creating user directories
 
 echo "creating user directories..."
-
+sleep 1
 xdg-user-dirs-update
 
 
 # setting up additional kernel modules for openrgb 
 
 echo "setting up additional kernel modules for openrgb..."
-
+sleep 1
 touch /etc/modules-load.d/i2c.conf && echo "i2c-dev" >> /etc/modules-load.d/i2c.conf
+sleep 1
 touch /etc/modules-load.d/i2c-piix4.conf && echo "i2c-piix4" >> /etc/modules-load.d/i2c-piix4.conf
 
 # creating additional groups and adding the user to it
 
 echo "creating additional groups and adding the user to it..."
-
+sleep 1
 groupadd --system i2c
+sleep 1
 usermod -aG i2c,kvm,libvirt,bluetooth,socklog joppe
 
 # setting up flatpak and flathub
 
 echo "setting up flatpak and flathub..."
-
-xbps-install -Sy flatpak 
+sleep 1
+xbps-install -Sy flatpak
+sleep 1 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # setting up swapfile
 
 echo "setting up swapfile..."
-
+sleep 1
 dd if=/dev/zero of=/swapfile bs=1M count=8k status=progress
+sleep 1
 chmod 0600 /swapfile
+sleep 1
 mkswap -U clear /swapfile
+sleep 1
 swapon /swapfile
+sleep 1
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
 # configure grub
 
 echo "configuring grub..."
-
+sleep 1
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4"/GRUB_CMDLINE_LINUX_DEFAULT="amdgpu.ppfeaturemask=0xffffffff amd_iommu=on iommu=pt loglevel=4"/' /etc/default/grub
+sleep 1
 sed -i 's/#GRUB_GFXMODE=1920x1080x32/GRUB_GFXMODE=3440x1440x32,1920x1080x32,auto/' /etc/default/grub
+sleep 1
 wget https://github.com/AdisonCavani/distro-grub-themes/raw/master/themes/void-linux.tar -P /home/joppe/Downloads
+sleep 1
 mkdir /home/joppe/Downloads/void-grub-theme
+sleep 1
 tar -xvf /home/joppe/Downloads/void-linux.tar -C /home/joppe/Downloads/void-grub-theme
+sleep 1
 mkdir /boot/grub/themes
+sleep 1
 cp -r /home/joppe/Downloads/void-grub-theme /boot/grub/themes/void
+sleep 1
 echo 'GRUB_THEME="/boot/grub/themes/void/theme.txt"' >> /etc/default/grub
+sleep 1
 update-grub
 
 # git cloning configs from repo and symlinking them to directories
 
 echo "git cloning configs from repo and copying them to directories..."
-
+sleep 1
 mkdir /home/joppe/repos
+sleep 1
 git clone git@github.com:juipeltje/configs.git /home/joppe/repos/configs
+sleep 1
 rm /home/joppe/.bashrc
+sleep 1
 ln -s /home/joppe/repos/configs/dotfiles/.bashrc /home/joppe/
+sleep 1
 ln -s /home/joppe/repos/configs/workstation/dotfiles/.Xresources /home/joppe
+sleep 1
 ln -s /home/joppe/repos/configs/workstation/dotfiles/dotconfig/qtile/ /home/joppe/.config/
+sleep 1
 ln -s /home/joppe/repos/configs/workstation/dotfiles/dotconfig/alacritty/ /home/joppe/.config/
+sleep 1
 ln -s /home/joppe/repos/configs/workstation/dotfiles/dotconfig/rofi/ /home/joppe/.config/
+sleep 1
 ln -s /home/joppe/repos/configs/workstation/dotfiles/dotconfig/dunst/ /home/joppe/.config/
+sleep 1
 ln -s /home/joppe/repos/configs/workstation/dotfiles/dotconfig/picom/ /home/joppe/.config/
+sleep 1
 cp -r /home/joppe/repos/configs/workstation/config-files/etc/X11/xorg.conf.d /etc/X11/
+sleep 1
 cp -r /home/joppe/repos/configs/workstation/config-files/etc/pipewire /etc/
+sleep 1
 cp -rf /home/joppe/repos/configs/config-files/etc/lightdm/lightdm.conf /etc/lightdm/
+sleep 1
 cp -rf /home/joppe/repos/configs/config-files/etc/lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/
+sleep 1
 cp -rf /home/joppe/repos/configs/config-files/etc/elogind /etc/
 
 # installing fonts
 
 echo "installing fonts..."
-
+sleep 1
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Mononoki.zip -P /home/joppe/Downloads/
+sleep 1
 mkdir /home/joppe/Downloads/Mononoki-Nerd-Font
+sleep 1
 unzip /home/joppe/Downloads/Mononoki.zip -d /home/joppe/Downloads/Mononoki-Nerd-Font/
+sleep 1
 cp -r /home/joppe/Downloads/Mononoki-Nerd-Font /usr/share/fonts/
+sleep 1
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Ubuntu.zip -P /home/joppe/Downloads/
+sleep 1
 mkdir /home/joppe/Downloads/Ubuntu-Nerd-Font 
+sleep 1
 unzip /home/joppe/Downloads/Ubuntu.zip -d /home/joppe/Downloads/Ubuntu-Nerd-Font/
+sleep 1
 cp -r /home/joppe/Downloads/Ubuntu-Nerd-Font /usr/share/fonts/
 
 # installing themes
 
 echo "installing themes..."
-
+sleep 1
 wget https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic.tar.xz -P /home/joppe/Downloads
+sleep 1
 tar -xvf /home/joppe/Downloads/Nordic.tar.xz -C /home/joppe/Downloads
+sleep 1
 cp -r /home/joppe/Downloads/Nordic /usr/share/themes
+sleep 1
 mkdir -p /home/joppe/kvantum-themes/Nordic
+sleep 1
 wget https://raw.githubusercontent.com/EliverLara/Nordic/master/kde/kvantum/Nordic/Nordic.kvconfig -P /home/joppe/kvantum-themes/Nordic/
+sleep 1
 wget https://raw.githubusercontent.com/EliverLara/Nordic/master/kde/kvantum/Nordic/Nordic.svg -P /home/joppe/kvantum-themes/Nordic/
+sleep 1
 git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk /home/joppe/gruvbox-material-gtk
+sleep 1
 cp -r /home/joppe/gruvbox-material-gtk/themes/Gruvbox-Material-Dark /usr/share/themes/
+sleep 1
 cp -r /home/joppe/gruvbox-material-gtk/icons/Gruvbox-Material-Dark /usr/share/icons/
+sleep 1
 wget https://github.com/theglitchh/Gruvbox-Kvantum/releases/download/v1.1/gruvbox-kvantum-v1.1.zip -P /home/joppe/Downloads
+sleep 1
 unzip gruvbox-kvantum-v1.1.zip -d /home/joppe/kvantum-themes/
+sleep 1
 cp -r /home/joppe/repos/configs/config-files/usr/share/icons/capitaine-cursors-light /usr/share/icons/
-
+sleep 1
 wget https://github.com/eromatiya/lightdm-webkit2-theme-glorious/releases/download/v2.0.5/lightdm-webkit2-theme-glorious-2.0.5.tar.gz -P /home/joppe/Downloads/
+sleep 1
 mkdir /home/joppe/Downloads/lightdm-webkit2-theme-glorious
+sleep 1
 tar -xvf /home/joppe/Downloads/lightdm-webkit2-theme-glorious-2.0.5.tar.gz -C /home/joppe/Downloads/lightdm-webkit2-theme-glorious/
+sleep 1
 cp -r /home/joppe/Downloads/lightdm-webkit2-theme-glorious /usr/share/lightdm-webkit/themes/glorious
 
 # setting up permissions for liquidctl
 
 echo "setting up permissions for liquidctl..."
-
+sleep 1
 wget https://raw.githubusercontent.com/liquidctl/liquidctl/main/extra/linux/71-liquidctl.rules -P /etc/udev/rules.d/
+
+# setting up weekly cronjob for SSD trimming
+
+echo "setting up weekly cronjob for SSD trimming..."
+sleep 1
+cp /home/joppe/repos/configs/Scripts/fstrim.sh /etc/cron.weekly/
 
 # enabling runit services
 
 echo "enabling runit services..."
-
+sleep 1
 ln -s /etc/sv/NetworkManager /var/service/
+sleep 1
 ln -s /etc/sv/dbus /var/service/
+sleep 1
 ln -s /etc/sv/bluetoothd /var/service/
+sleep 1
 ln -s /etc/sv/ntpd /var/service/
+sleep 1
 ln -s /etc/sv/socklog-unix /var/service/
+sleep 1
 ln -s /etc/sv/nanoklogd /var/service/
+sleep 1
 ln -s /etc/sv/libvirtd /var/service/
+sleep 1
 ln -s /etc/sv/cronie /var/service/
+sleep 1
+ln -s /etc/sv/sshd /var/service/
+sleep 1
 ln -s /etc/sv/lightdm /var/service/
 sv down lightdm
 
 # fixing any ownership issues for the user's home folder
 
 echo "fixing any ownership issues for the user's home folder..."
+sleep 1
+chown -R joppe /home/joppe
 
-chown joppe /home/joppe
+# fixing dbus issues in window manager session
+
+echo "fixing dbus issues in window manager session..."
+sleep 1
+sed -i 's/Exec=qtile start/Exec=dbus-launch --exit-with-session qtile start/' /usr/share/xsessions/qtile.desktop
 
 echo "Finished!! You can now reboot your machine."
