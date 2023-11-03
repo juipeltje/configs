f6cc3add2498b019fdf443e080a0f7c5aa30a795
juipeltje <joppe4444@outlook.com>
2023-11-03 21:53:25 +0100

    adding separate yoda script for the liquidctl nix package, changing the qtile autostart script accordingly, and adding new configs for wayland applications

---
+++ workstation/dotfiles/dotconfig/hypr/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# autostart script for hyprland
+
+hyprpaper &
+sleep 1
+waybar &
+sleep 1
+hyprctl setcursor capitaine-cursors-light 24 &
--- workstation/dotfiles/dotconfig/qtile/autostart.sh
+++ workstation/dotfiles/dotconfig/qtile/autostart.sh
@@ -13,8 +13,10 @@ wireplumber &
 pipewire-pulse &
 playerctld daemon &
 /usr/bin/lxpolkit &
-/home/joppe/repos/configs/Scripts/liquidctl.sh &
-/home/joppe/repos/configs/Scripts/yoda.sh --match commander control sync with "(40,40),(50,45),(55,50),(60,60),(65,70),(70,80),(80,90),(85,100)" on k10temp.tctl &
+liquidctl initialize all &
+sleep 2
+liquidctl --match kraken set pump speed 100 &
+/home/joppe/repos/configs/Scripts/yoda-nix.sh --match commander control sync with "(40,40),(50,45),(55,50),(60,60),(65,70),(70,80),(80,90),(85,100)" on k10temp.tctl &
 xset s off -dpms &
 nm-applet &
 openrgb &
