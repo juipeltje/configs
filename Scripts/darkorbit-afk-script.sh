a332f7c215fc90ab40760487801414acc47716c6
juipeltje <joppe4444@outlook.com>
2023-12-28 16:18:08 +0100

    improving darkorbit macro script and changing alias in bashrc

--- Scripts/darkorbit-afk-script.sh
+++ Scripts/darkorbit-afk-script.sh
@@ -1,9 +1,8 @@
- 
 #!/bin/bash
 
+# macro script to stay afk in darkorbit
+ 
 while true; do
-  xdotool key 2
-  sleep $((RANDOM % 90))
-  xdotool key 1
-  sleep $((RANDOM % 90))
+  xdotool key $((1 + RANDOM % 7)) 
+  sleep $((2 + RANDOM % 90))
 done
