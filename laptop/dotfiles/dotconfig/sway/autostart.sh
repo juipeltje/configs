ca737da930d4db757771d1e102a49c08fb79af10
juipeltje <joppe4444@outlook.com>
2024-01-05 17:01:03 +0100

    changing swaybg command in favor of swaymsg to set background in sway autostart

--- laptop/dotfiles/dotconfig/sway/autostart.sh
+++ laptop/dotfiles/dotconfig/sway/autostart.sh
@@ -2,7 +2,7 @@
 
 # autostart script for swayfx
 
-swaybg -i /home/joppe/Pictures/wallpapers/kerst/kerst-1.jpg -m fill &
+swaymsg output "*" bg /home/joppe/Pictures/wallpapers/games/Gravity-Rush/gravity-rush-3.jpg fill &
 waybar -c /home/joppe/.config/waybar/sway-config -s /home/joppe/.config/waybar/sway-style.css &
 mako -c /home/joppe/.config/mako/sway-config &
 pipewire &
