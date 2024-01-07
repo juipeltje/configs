c38f1d66b760a11a88beb3681b391edc3d4af01e
juipeltje <joppe4444@outlook.com>
2024-01-07 16:09:32 +0100

    adding xrandr command to set primary monitor for games running under xwayland in sway autostart script

--- workstation/dotfiles/dotconfig/sway/autostart.sh
+++ workstation/dotfiles/dotconfig/sway/autostart.sh
@@ -14,3 +14,5 @@ liquidctl --match kraken set pump speed 100 &
 /home/joppe/repos/configs/Scripts/yoda --match commander control sync with "(40,40),(50,45),(55,50),(60,60),(65,70),(70,80),(80,90),(85,100)" on k10temp.tctl &
 nm-applet &
 openrgb &
+xrandr --output DP-1 --primary &
+
