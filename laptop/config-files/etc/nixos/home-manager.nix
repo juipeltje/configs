28b5d103b3ed84de7e6a21ab774d5479065834ae
juipeltje <joppe4444@outlook.com>
2024-04-04 16:00:54 +0200

    fixing laptop bash aliases in home-manager.nix

--- laptop/config-files/etc/nixos/home-manager.nix
+++ laptop/config-files/etc/nixos/home-manager.nix
@@ -17,9 +17,9 @@
       bash = {
         enable = true;
         shellAliases = {
-          rebuild = "sudo nixos-rebuild boot --flake /home/joppe/repos/configs/workstation/config-files/etc/nixos/";
-          rebuild-switch = "sudo nixos-rebuild switch --flake /home/joppe/repos/configs/workstation/config-files/etc/nixos/";
-          update = "sudo nix flake update /home/joppe/repos/configs/workstation/config-files/etc/nixos/ && sudo nixos-rebuild switch --flake /home/joppe/repos/configs/workstation/config-files/etc/nixos/";
+          rebuild = "sudo nixos-rebuild boot --flake /home/joppe/repos/configs/laptop/config-files/etc/nixos/";
+          rebuild-switch = "sudo nixos-rebuild switch --flake /home/joppe/repos/configs/laptop/config-files/etc/nixos/";
+          update = "sudo nix flake update /home/joppe/repos/configs/laptop/config-files/etc/nixos/ && sudo nixos-rebuild switch --flake /home/joppe/repos/configs/laptop/config-files/etc/nixos/";
           cleanup = "sudo nix-collect-garbage && sudo nix-store --optimise";
           full-cleanup = "sudo nix-collect-garbage && sudo nix-collect-garbage -d && sudo nix-store --optimise";
           alsa-store = "alsactl --file ~/.config/asound.state store";
