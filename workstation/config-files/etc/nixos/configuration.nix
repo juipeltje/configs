3185abe04b58f736689ef31ed9b8c2b4706844fd
juipeltje <joppe4444@outlook.com>
2024-04-23 21:38:40 +0200

    moving greeter configuration from configuration.nix into its own separate file

--- workstation/config-files/etc/nixos/configuration.nix
+++ workstation/config-files/etc/nixos/configuration.nix
@@ -2,37 +2,12 @@
 
 { config, pkgs, ... }:
 
-let 
-    HyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
-      # Hyprland greeter config
-
-      # monitor settings
-      monitor=DP-1,3440x1440@165,0x1080,1
-      monitor=DP-2,disable
-      #monitor=DP-2,2560x1080@75,0x0,1
-      # Execute your favorite apps at launch
-      exec-once = ${pkgs.greetd.regreet}/bin/regreet; hyprctl dispatch exit
-
-      # Env variables/set cursor size
-      env = XCURSOR_SIZE,24
-
-      input {
-        follow_mouse = 1
-        sensitivity = 0
-      }
-
-      misc {
-	disable_hyprland_logo = true
-	disable_splash_rendering = true
-	force_default_wallpaper = 0
-      }
-    '';
-  in
-
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      # greeter settings
+      ./greeter.nix
       # Import home-manager settings
       ./home-manager.nix
     ];
@@ -161,41 +136,6 @@ let
         "context.properties" = {
           "default.clock.allowed-rates" = [ 44100 48000 96000 ];
         };
-      };
-    };
-  };
-  
-  # Enable greetd and ReGreet, and greetd/ReGreet configuration.
-  services.greetd = {
-    enable = true;
-    vt = 1;
-    settings = {
-      default_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland --config ${HyprlandConfig}"; 
-        user = "greeter";     
-      };
-    };
-  };
-
-  programs.regreet = {
-    enable = true;
-    settings = {
-      background = {
-        path = /greeter-wallpapers/1.jpg;
-        fit = "Contain";
-      };
-      
-      GTK = {
-        application_prefer_dark_theme = false;
-        cursor_theme_name = "phinger-cursors-light";
-        font_name = "Ubuntu Nerd Font Medium 12";
-        icon_theme_name = "Mint-Y-Sand";
-        theme_name = "Tokyonight-Dark-BL";
-      };
-
-      commands = {
-        reboot = [ "systemctl" "reboot" ];
-        poweroff = [ "systemctl" "poweroff" ];
       };
     };
   };
---
+++ workstation/config-files/etc/nixos/greeter.nix
@@ -0,0 +1,69 @@
+# NixOS greeter configuration
+
+{ config, pkgs, ... }:
+
+# Hyprland configuration for greeter
+let
+  HyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
+    # Hyprland greeter config
+
+    # monitor settings
+    monitor=DP-1,3440x1440@165,0x1080,1
+    monitor=DP-2,disable
+    #monitor=DP-2,2560x1080@75,0x0,1
+    # Execute your favorite apps at launch
+    exec-once = ${pkgs.greetd.regreet}/bin/regreet; hyprctl dispatch exit
+
+    # Env variables/set cursor size
+    env = XCURSOR_SIZE,24
+
+    input {
+      follow_mouse = 1
+      sensitivity = 0
+    }
+
+    misc {
+      disable_hyprland_logo = true
+      disable_splash_rendering = true
+      force_default_wallpaper = 0
+    }
+  '';
+in
+
+{
+
+  # Enable greetd and ReGreet, and greetd/ReGreet configuration.
+  services.greetd = {
+    enable = true;
+    vt = 1;
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland --config ${HyprlandConfig}";
+        user = "greeter";
+      };
+    };
+  };
+
+  programs.regreet = {
+    enable = true;
+    settings = {
+      background = {
+        path = /greeter-wallpapers/1.jpg;
+        fit = "Contain";
+      };
+
+      GTK = {
+        application_prefer_dark_theme = false;
+        cursor_theme_name = "phinger-cursors-light";
+        font_name = "Ubuntu Nerd Font Medium 12";
+        icon_theme_name = "Mint-Y-Sand";
+        theme_name = "Tokyonight-Dark-BL";
+      };
+
+      commands = {
+        reboot = [ "systemctl" "reboot" ];
+        poweroff = [ "systemctl" "poweroff" ];
+      };
+    };
+  };
+}
