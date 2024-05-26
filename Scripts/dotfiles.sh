e737cbbf98e58c74f4e24c1d3487e1010f62f2f6
juipeltje <joppe4444@outlook.com>
2024-05-26 21:24:09 +0200

    adding xinitrc files to remove function in dotfile install script.

--- Scripts/dotfiles.sh
+++ Scripts/dotfiles.sh
@@ -29,6 +29,8 @@ options_2=(
 rm_default_configs() {
 	rm -rf /home/${user}/.bashrc
 	rm -rf /home/${user}/.bash_profile
+	rm -rf /home/${user}/.xinitrc-i3
+	rm -rf /home/${user}/.xinitrc-qtile
 	rm -rf /home/${user}/.Xresources
 	rm -rf /home/${user}/.config/alacritty
 	rm -rf /home/${user}/.config/dunst
