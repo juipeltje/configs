bf1861f5bccd30e09f0d9b4e77363f6846d4a843
juipeltje <joppe4444@outlook.com>
2024-05-30 16:35:14 +0200

    adding keybind for theme switcher script to sway, adding color include line to laptop sway config, improving sway theme switcher script (wip), and renaming waybar laptop style configs.

--- common/home/dotconfig/sway/wofi-theme-switcher.sh
+++ common/home/dotconfig/sway/wofi-theme-switcher.sh
@@ -5,19 +5,19 @@
 theme=$( echo -e "Nordic\nGruvbox-Material-Dark\nTokyo-Night" | wofi --show dmenu)
 
 case $theme in
-	nordic)
-		notify-send "Switching to Nordic theme..."
+	Nordic)
 		sed -i 's|^include ~/.config/sway/colors.*|include ~/.config/sway/colors-nordic.conf|' ~/.config/sway/config
 		swaymsg reload
 		sed -i 's|^import.*|import = ["~/.config/alacritty/nordic.toml"]|' ~/.config/alacritty/alacritty.toml
 		killall mako
 		mako -c ~/.config/mako/nordic-config &
 		killall waybar
-		waybar -c ~/.config/waybar/sway-nordic-config -s ~/.config/waybar/sway-style-nordic.css &
+		waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-nordic.css &
+		sed -i 's|^mako.*|mako -c ~/.config/mako/nordic-config \&|' ~/.config/sway/autostart.sh
+		sed -i 's|^waybar.*|waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-nordic.css \&|' ~/.config/sway/autostart.sh
 		notify-send "Current theme: Nordic"
 		;;
-	gruvbox-material-dark)
-		notify-send "Switching to Gruvbox-Material-Dark theme..."
+	Gruvbox-Material-Dark)
                 sed -i 's|^include ~/.config/sway/colors.*|include ~/.config/sway/colors-gruvbox-material-dark.conf|' ~/.config/sway/config
                 swaymsg reload
                 sed -i 's|^import.*|import = ["~/.config/alacritty/gruvbox-material-dark.toml"]|' ~/.config/alacritty/alacritty.toml
@@ -25,17 +25,20 @@ case $theme in
                 mako -c ~/.config/mako/gruvbox-material-dark-config &
                 killall waybar
                 waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-gruvbox-material-dark.css &
+		sed -i 's|^mako.*|mako -c ~/.config/mako/gruvbox-material-dark-config \&|' ~/.config/sway/autostart.sh
+		sed -i 's|^waybar.*|waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-gruvbox-material-dark.css \&|' ~/.config/sway/autostart.sh
 		notify-send "Current theme: Gruvbox-Material-Dark"
                 ;;
-	tokyo-night)
-		notify-send "Switching to Tokyo-Night theme..."
-                sed -i 's|^include ~/.config/sway/colors.*|include ~/.config/sway/colors-tokyonight.conf|' ~/.config/s>
+	Tokyo-Night)
+                sed -i 's|^include ~/.config/sway/colors.*|include ~/.config/sway/colors-tokyonight.conf|' ~/.config/sway/config
                 swaymsg reload
-                sed -i 's|^import.*|import = ["~/.config/alacritty/tokyo-night.toml"]|' ~/.config/alacritty/alacritty.t>
+                sed -i 's|^import.*|import = ["~/.config/alacritty/tokyo-night.toml"]|' ~/.config/alacritty/alacritty.toml
                 killall mako
                 mako -c ~/.config/mako/tokyonight-config &
                 killall waybar
                 waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-tokyonight.css &
+		sed -i 's|^mako.*|mako -c ~/.config/mako/tokyonight-config \&|' ~/.config/sway/autostart.sh
+		sed -i 's|^waybar.*|waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-tokyonight.css \&|' ~/.config/sway/autostart.sh
 		notify-send "Current theme: Tokyo-Night"
                 ;;
 esac
