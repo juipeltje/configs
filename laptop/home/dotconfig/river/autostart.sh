9a7b2376fbcc0d88ffe09677b6376f25e667a8df
juipeltje <joppe4444@outlook.com>
2024-07-01 21:22:34 +0200

    adding swaybg command to river autostart and adding touchpad settings to river init (laptop).

--- laptop/home/dotconfig/river/autostart.sh
+++ laptop/home/dotconfig/river/autostart.sh
@@ -2,8 +2,9 @@
 
 # autostart script for River
 
-waybar -c ~/.config/waybar/river-config -s ~/.config/waybar/river-style-tokyonight.css &
-mako -c ~/.config/mako/tokyonight-config &
+swaybg -i ~/Pictures/sway/background.jpg -m fill &
+waybar -c ~/.config/waybar/river-config -s ~/.config/waybar/river-style-gruvbox-material-dark.css &
+mako -c ~/.config/mako/gruvbox-material-dark-config &
 pipewire &
 playerctld daemon &
 lxsession &
