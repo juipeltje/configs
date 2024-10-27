ae844f7ae8b2a36f9a34d7fdfc24d8b47fc607bf
juipeltje <joppe4444@outlook.com>
2024-10-27 16:56:50 +0100

    pinning kernel version to 6.10 for workstation (until nix stable uses 6.7 or up).

--- nixos/workstation/packages.nix
+++ nixos/workstation/packages.nix
@@ -6,7 +6,7 @@
   
   # Select kernel version.
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_10;
   };
 
   # workstation-specific packages installed in system profile.
