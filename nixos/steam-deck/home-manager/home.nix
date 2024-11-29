37ac443c3be555992bd0a8575ede727e1f8db374
juipeltje <joppe4444@outlook.com>
2024-11-29 22:30:41 +0100

    reverting previous change

--- nixos/steam-deck/home-manager/home.nix
+++ nixos/steam-deck/home-manager/home.nix
@@ -32,8 +32,8 @@
     package = pkgs.nix;
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      extra-substituters = [ "https://ezkea.cachix.org/" ];
-      extra-trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+      substituters = [ "https://ezkea.cachix.org/" ];
+      trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
     };
   };
 
