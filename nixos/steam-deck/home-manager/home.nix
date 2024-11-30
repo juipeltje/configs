9b285e21d46d1a524de9365512ef2cc7b5f2ada9
juipeltje <joppe4444@outlook.com>
2024-11-30 11:32:36 +0100

    prepending extra to substituters and trusted-public-keys in steam deck home.nix

--- nixos/steam-deck/home-manager/home.nix
+++ nixos/steam-deck/home-manager/home.nix
@@ -32,8 +32,8 @@
     package = pkgs.nix;
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = [ "https://ezkea.cachix.org/" ];
-      trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+      extra-substituters = [ "https://ezkea.cachix.org/" ];
+      extra-trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
     };
   };
 
