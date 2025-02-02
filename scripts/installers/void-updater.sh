4f6695fb9be4caf790a313a857f47eb3ae6d57a3
juipeltje <joppe4444@outlook.com>
2025-02-02 22:17:54 +0100

    adding echo commands to void-updater script to let user know what it is doing.

--- scripts/installers/void-updater.sh
+++ scripts/installers/void-updater.sh
@@ -6,21 +6,33 @@ set -e
 
 # Variables
 user=joppe
+color_reset="\033[0m"
+green="\033[0;32m"
 
 # Update xbps package manager
+echo -e "${green}Starting Void-updater script... (current user is '${user}')${color_reset}"
+echo -e "${green}Checking if the xbps package need to be updated...${color_reset}"
 xbps-install -u xbps
 
 # Update packages with xbps
+echo -e "${green}Checking for updates with xbps...${color_reset}"
 xbps-install -Su
 
 # Replacing desktop session shortcuts in case they are overwritten by update
+echo -e "${green}Replacing desktop session shortcuts in case they are overwritten by update...${color_reset}"
 cp -f /home/${user}/repos/configs/common/usr/share/xsessions/i3.desktop /usr/share/xsessions/
 cp -f /home/${user}/repos/configs/common/usr/share/xsessions/qtile.desktop /usr/share/xsessions/
 cp -f /home/${user}/repos/configs/common/usr/share/wayland-sessions/river.desktop /usr/share/wayland-sessions/
 cp -f /home/${user}/repos/configs/common/usr/share/wayland-sessions/sway.desktop /usr/share/wayland-sessions/
 
 # Check for services that need to be restarted with xcheckrestart
+echo -e "${green}Checking for services that need to be restarted with xcheckrestart...${color_reset}"
+echo -e "${green}The following services need to be restarted:${color_reset}"
 sudo -u ${user} xcheckrestart
 
 # Update Flatpaks
+echo -e "${green}Checking for flatpak updates...${color_reset}"
 sudo -u ${user} flatpak update
+
+# Finished
+echo -e "${green}Finished! system is fully updated.${color_reset}"
