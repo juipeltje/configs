07da0040a145be1387f39bcac6b4dc337959697f
juipeltje <joppe4444@outlook.com>
2025-02-02 17:01:56 +0100

    changing height of terminal scratchpad in laptop river config.

--- laptop/home/dotconfig/sway/autostart.sh
+++ laptop/home/dotconfig/sway/autostart.sh
@@ -5,8 +5,8 @@
 kitty --class scratchpad &
 pipewire &
 autotiling --splitratio 1.61 --limit 2 &
-waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-nordic.css &
-mako -c ~/.config/mako/nordic-config &
+waybar -c ~/.config/waybar/sway-config -s ~/.config/waybar/sway-style-gruvbox-material-dark.css &
+mako -c ~/.config/mako/gruvbox-material-dark-config &
 playerctld daemon &
 lxsession &
 nm-applet &
