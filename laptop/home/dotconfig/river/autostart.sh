30862ab233139d9ab15c240999d5e093b83e26d2
juipeltje <joppe4444@outlook.com>
2025-02-14 21:53:40 +0100

    changing sway laptop input config to use generic type:touchpad identifier instead of using the specific device identifier.

--- laptop/home/dotconfig/river/autostart.sh
+++ laptop/home/dotconfig/river/autostart.sh
@@ -6,7 +6,7 @@ pipewire &
 swaybg -i ~/Pictures/sway/background.jpg -m fill &
 sed -i --follow-symlinks 's|"include".*|"include": "~/.config/waybar/modules-river",|' ~/.config/waybar/config
 waybar &
-mako -c ~/.config/mako/gruvbox-material-dark-config &
+mako -c ~/.config/mako/nordic-config &
 playerctld daemon &
 lxsession &
 nm-applet &
