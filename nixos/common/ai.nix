c6df1c28522e30c87ec2b9dde18ae1d5a743c631
juipeltje <joppe4444@outlook.com>
2025-04-08 16:48:56 +0200

    adding systemd user service to start ollama serve to ai.nix.

--- nixos/common/ai.nix
+++ nixos/common/ai.nix
@@ -10,4 +10,21 @@
   environment.systemPackages = with pkgs; [
     ollama-rocm
   ];
+
+  # create ollama systemd user service
+  systemd.user.services.ollama = {
+    enable = true;
+    unitConfig = {
+      Description = "Start ollama serve";
+      After = [ "network.target" ];
+    };
+
+    serviceConfig = {
+      ExecStart = "${pkgs.ollama-rocm}/bin/ollama serve";
+      Restart = "on-failure";
+    };
+
+    environment = if "${config.networking.hostName}" == "NixOS-Rig" then { HSA_OVERRIDE_GFX_VERSION = "10.3.0"; } else { };
+    wantedBy = [ "default.target" ];
+  };
 }
