eac87e31690e108f4f09eeef8450bc5df7fca831
juipeltje <joppe4444@outlook.com>
2025-07-14 22:02:35 +0200

    removing bottles and changing heroic package to unwrapped version in packages.nix, and adding flake.lock.

--- nix/machines/common/gaming.nix
+++ nix/machines/common/gaming.nix
@@ -7,8 +7,7 @@
   home.packages = with pkgs; [
     # Wine/gamelaunchers
     steam
-    bottles
-    heroic
+    heroic-unwrapped # unwrapped version seems to be required in order for games to be able to launch on non-NixOS. might also be required for other packages?
     prismlauncher
     mcpelauncher-ui-qt
   
