f82c9aeb21919c298ddec071810c0699bc1467fa
juipeltje <joppe4444@outlook.com>
2026-01-04 22:14:45 +0100

    adding cron script for podman-compose, and editing crontab to start podman-compose script at boot, and removing jellyfin and navidrome scripts from crontab.

---
+++ scripts/cron/server/podman-compose.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Script that starts podman-compose with the required flags, and retries if it fails.
+
+# redirect stderr and stdout to logfile.
+user=joppe
+log=/home/${user}/log/podman-compose/$(date +%d-%m-%Y).log
+exec 1>>${log} 2>&1
+
+until /home/${user}/.nix-profile/bin/podman-compose -f /home/${user}/.config/containers/compose.yml up -d; do
+  echo "podman-compose failed! if this keeps happening, please check your logs."
+  sleep 1
+done
