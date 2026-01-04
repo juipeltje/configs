1c8896b525d33e1d46a9f7ff1505bccce926f71e
juipeltje <joppe4444@outlook.com>
2026-01-04 22:28:38 +0100

    removing ollama env entirely from compose.yml, removing full path to podman-compose binary in podman-compose.sh, and adding path to crontab.txt.

--- scripts/cron/server/podman-compose.sh
+++ scripts/cron/server/podman-compose.sh
@@ -7,7 +7,7 @@ user=joppe
 log=/home/${user}/log/podman-compose/$(date +%d-%m-%Y).log
 exec 1>>${log} 2>&1
 
-until /home/${user}/.nix-profile/bin/podman-compose -f /home/${user}/.config/containers/compose.yml up -d; do
+until podman-compose -f /home/${user}/.config/containers/compose.yml up -d; do
   echo "podman-compose failed! if this keeps happening, please check your logs."
   sleep 1
 done
