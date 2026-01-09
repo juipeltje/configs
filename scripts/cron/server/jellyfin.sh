d7076817afbef1ba94b0bf3dd489fc6fd692c7b3
juipeltje <joppe4444@outlook.com>
2026-01-09 17:48:25 +0100

    changing user variable from jellyfin to gpu in jellyfin.sh, and changing path to script from jellyfin home folder to gpu homefolder in gpu-crontab.txt.

--- scripts/cron/server/jellyfin.sh
+++ scripts/cron/server/jellyfin.sh
@@ -3,7 +3,7 @@
 # Script that starts Jellyfin with the required flags, and sends logs to a file.
 
 # redirect stderr and stdout to logfile.
-user=jellyfin
+user=gpu
 log=/home/${user}/log/jellyfin/$(date +%d-%m-%Y).log
 JELLYFINDIR="/12TB-HDD/jellyfin/"
 exec 1>>${log} 2>&1
